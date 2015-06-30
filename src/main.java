f36a8388f6532b4bfbe4872215a92d5edd973bcd
teksta7 <thomp194@uni.coventry.ac.uk>
2015-06-30 12:27:45 +0100

    #comment Java in headless mode to help GUI

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("java.awt.headless", "true");
 		System.out.println("Opened OK");
 		GUI graphics = new GUI();
 		graphics.Display();
